a2fdc02e87b50fa167bd4564f8c53fb3eadb34af
Oladokun Oladipupo <ola@5centscdn.com>
2023-03-16 22:29:05 +0100

    https://github.com/Adebayo114/practical.git

---
+++ script.js
@@ -0,0 +1,17 @@
+// let myName = "dayo"
+// let myHouse = "duplex"
+// let myAge = 15
+// let myHeight = 67
+// console.log(`my Name is ${myName}, I live in a  ${myHouse}, i am ${myAge} years old, with the height of ${myHeight}.`)
+
+// var Myvar = 15
+// // Myvar++
+// // Myvar--
+// Myvar += 2
+// console.log(Myvar)
+
+// document.getElementById("demo").innerHTML = myfunction(dayo)
+// function myfunction(name){
+// return "hello" + name;
+// }
+
